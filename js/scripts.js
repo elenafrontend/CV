88c38dc900d032def8d5a36341b005c916c5d1c4
elena.g <gumerova.e.p@ya.ru>
2021-05-06 14:07:40 +0300

    scripts: burger menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,44 @@
-// typing text
+"use strict";
+
+let header = document.querySelector(".header");
+let burgerBtn = document.querySelector(".burger");
+let navLinks = document.querySelectorAll(".nav__link");
+
+//-----------  burger-menu  -----------
+
+burgerBtn.addEventListener("click", function () {
+  header.classList.toggle("header--active-nav");
+  burgerBtn.classList.toggle("burger--active");
+
+  // блокируем/возобновляем скролл страницы
+  if (burgerBtn.classList.contains("burger--active")) {
+    document.body.style.overflow = "hidden";
+  } else {
+    document.body.style.overflow = "";
+  }
+});
+
+// функция закрытия меню бургер
+let resetNav = function () {
+  if (burgerBtn.classList.contains("burger--active")) {
+    burgerBtn.classList.remove("burger--active");
+    header.classList.remove("header--active-nav");
+    document.body.style.overflow = "";
+  }
+};
+
+// закрываем бургер при клике на ссылку в меню
+for (let navLink of navLinks) {
+  navLink.addEventListener("click", function () {
+    // если открыто меню-бургер, закрываем его
+    resetNav();
+  });
+}
+
+// закрываем бургер меню при ресайзе окна
+window.addEventListener("resize", resetNav);
+
+//-----------  typing text  -----------
 // https://mattboldt.com/demos/typed-js/
 
 var typed = new Typed(".typed", {
@@ -12,7 +52,7 @@ var typed = new Typed(".typed", {
   },
 });
 
-// 3D cards
+//-----------  3D cards  -----------
 // https://micku7zu.github.io/vanilla-tilt.js/
 
 VanillaTilt.init(document.querySelectorAll(".portfolio__card"), {
